58362adbde0fa36d5c6f9ae7d8ec6f31391c9be2
GJWP <84923442+Gjelyawp@users.noreply.github.com>
2022-08-17 18:57:49 +0300

    1

---
+++ TasksALL/src/main/java/pkg/Rectangle.java
@@ -0,0 +1,12 @@
+package pkg;
+
+public class Rectangle {
+	public double a,b;
+	public Rectangle(double a, double b){
+		this.a =a;
+		this.b =b;
+	}
+	public  double Calc (){
+		return this.a * this.b;
+	}
+}
---
+++ TasksALL/src/main/java/pkg/Square.java
@@ -0,0 +1,11 @@
+package pkg;
+
+public class Square {
+	public double l;
+	public Square(double l){
+		this.l = l;
+	}
+	public double Calc (){
+		return this.l * this.l;
+	}
+}
