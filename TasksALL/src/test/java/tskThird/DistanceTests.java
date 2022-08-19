607b7554cbea486ca34d24d91c8a62bc8331d6db
GJWP <84923442+Gjelyawp@users.noreply.github.com>
2022-08-19 16:34:50 +0300

    Update

--- TasksALL/src/test/java/tskThird/DistanceTests.java
+++ TasksALL/src/test/java/tskThird/DistanceTests.java
@@ -11,8 +11,6 @@ public class DistanceTests {
 	public void DistantTest () {
 		Points p1 = new Points(6, 7);
 		Points p2 = new Points(7, 8);
-		// Выделяю только один кейс на проверку правильности расчета.
-		// Проверка с округлением сомнительна, т.к проверяется не расчет, а округление на мой взгляд (добавил для наглядности)
 		double exResult = 1.4142135623730951;
 		try {
 			Assert.assertEquals(p2.Distance(p1), exResult);
