54048dc0529a3f2d56cca44a9de1fb5d6cdb6e03
GJWP <84923442+Gjelyawp@users.noreply.github.com>
2022-08-24 08:51:02 +0300

    Update tsk3

--- TasksALL/src/test/java/tskThird/DistanceTests.java
+++ TasksALL/src/test/java/tskThird/DistanceTests.java
@@ -8,44 +8,30 @@ import org.testng.annotations.Test;
 
 public class DistanceTests {
 	@Test
-	public void DistantTest () {
+	public void DistantTest() {
 		Points p1 = new Points(6, 7);
 		Points p2 = new Points(7, 8);
 		double exResult = 1.4142135623730951;
-		try {
-			Assert.assertEquals(p2.Distance(p1), exResult);
-			System.out.println("Расстояние вычисленно верно D=" + p2.Distance(p1));
-		} catch (AssertionError error) {
-			System.out.println("Расстояние между точками вычислено неверно D=" + p2.Distance(p1) + " " + "Правильное D=" + exResult);
-		}
+		Assert.assertEquals(p2.Distance(p1), exResult);
 	}
+
+
 	@Test
-	public void DistantTest2 () {
+	public void DistantTest2() {
 		//отрицательные значения
 		Points p1 = new Points(-6, 7);
 		Points p2 = new Points(-7, 9);
 		double exResult = 2.23606797749979;
-		try {
-			Assert.assertEquals(p2.Distance(p1), exResult);
-			System.out.println("Расстояние вычисленно верно D=" + p2.Distance(p1));
-		} catch (AssertionError error) {
-			System.out.println("Расстояние между точками вычислено неверно D=" + p2.Distance(p1) + " " + "Правильное D=" + exResult);
-		}
+		Assert.assertEquals(p2.Distance(p1), exResult);
 	}
+
 	@Test
-	public void DistantTest3 () {
+	public void DistantTest3() {
 		//с нулевой координатой
 		Points p1 = new Points(-6, 7);
 		Points p2 = new Points(1, 0);
 		double exResult = 9.899494936611665;
-		try {
-			Assert.assertEquals(p2.Distance(p1), exResult);
-			System.out.println("Расстояние вычисленно верно D=" + p2.Distance(p1));
-		} catch (AssertionError error) {
-			System.out.println("Расстояние между точками вычислено неверно D=" + p2.Distance(p1) + " " + "Правильное D=" + exResult);
-		}
+		Assert.assertEquals(p2.Distance(p1), exResult);
 	}
-
-
-	}
+}
 
